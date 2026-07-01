242d5ca3c006b624d83e194109bb8f1c58422f8b
Mouad-Mehdi <mouadmehdi2006@gmail.com>
2026-07-01 19:20:45 +0100

    Add files via upload

---
+++ utilities.py
@@ -0,0 +1,76 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# An implementation of the FTCS approximation:
+def heat_ftcs(I, N, r, T_initial):
+    # Checking the stability conditions
+    #if r > 0.5 :
+     #   raise ValueError("Stability condition violated! r must be <= 0.5. Current r = {r:.4f}")
+       
+    # Initializing the space time grid matrix:
+    T = np.zeros((I+1,N+1))
+
+    # Appplying the initial conditions:
+    T[:,0] = T_initial
+
+    # Forcing the boundary conditions:
+    T[0,:] = 0
+    T[I,:] = 0
+
+    # FTCS loop:
+    for n in range(0,N):
+        T[1:I, n+1] = (1 - 2*r) * T[1:I, n] + r * (T[2:I+1, n] + T[0:I-1, n])
+
+    return T
+
+# Ploting a heatmap:
+def heatmap(data,L,H):
+    plt.figure()
+    heat = plt.imshow(data, cmap='inferno', aspect="auto", origin='lower',extent=[0,H,0,L])
+    cbar = plt.colorbar(heat)
+    cbar.set_label("Temperature (T)", size=11)
+    plt.xlabel("Time (s)", fontsize=11)
+    plt.ylabel("Position along the Rod (x)", fontsize=11)
+    plt.tight_layout()
+    plt.show()
+
+
+# Comparing the analytical and numerical solutions via two heatmaps:
+def heatmap_comp(analytical, numerical, L, H, v_max):
+    # Calculate the absolute difference matrix:
+    error = np.abs(numerical - analytical)
+
+    # Create 1 row, 3 columns
+    fig, axes = plt.subplots(1, 3, figsize=(16, 5), sharey=True)
+
+    limits = [0, H, 0, L]
+
+    # Plot Numerical
+    im_num = axes[0].imshow(numerical, cmap='inferno', aspect='auto', 
+                        origin='lower', vmin=0, vmax=v_max, extent=limits)
+    axes[0].set_title("Numerical Solution")
+    axes[0].set_xlabel("Time (s)")
+    axes[0].set_ylabel("Position along the Rod (m)")
+
+    # Plot Analytical
+    axes[1].imshow(analytical, cmap='inferno', aspect='auto', 
+                origin='lower', vmin=0, vmax=v_max, extent=limits)
+    axes[1].set_title("Analytical Solution")
+    axes[1].set_xlabel("Time (s)")
+
+    # Creating the temperature color bar:
+    cbar_temp = fig.colorbar(im_num, ax=[axes[0], axes[1]])
+    cbar_temp.set_label("Temperature (T)", size=12)
+
+    # Absolute Error Heatmap 
+    im_err = axes[2].imshow(error, cmap='viridis', aspect='auto', origin='lower',
+                            extent=limits, vmin=0, vmax=error.max())
+    axes[2].set_title("Absolute Error |Num - Ana|")
+    axes[2].set_xlabel("Time (s)")
+
+    # Add a colorbar for the error heatmap
+    cbar_err = fig.colorbar(im_err, ax=axes[2])
+    cbar_err.set_label("Temperature difference in C", size=11)
+
+    plt.show()
+
